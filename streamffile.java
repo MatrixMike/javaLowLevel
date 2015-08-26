b031b73304193bc1cc361590cf3049ef32a1d60d
Michael Hewitt <matroxmike@icloud.com>
2015-08-26 22:52:08 +1000

    using more lambdas and streams 3

--- streamffile.java
+++ streamffile.java
@@ -83,12 +83,17 @@ public void stream_from_file() throws IOException {
 }
 
 	public static void main (String args[]) {
-		    int[] numbers = { 1, -25, 2, 3, 3, 3, 4, 5, 6, 7 };
+	int[] numbers = { 1, -25, 2, 3, 3, 3, 4, 5, 6, 7 };
+	double[] NUMBERS = {1, 24, 45, 62, 85, 8, 91, 3, 5, 56, 9};
 
     int sum = Arrays.stream(numbers)
 	.distinct()
     // .filter ( p -> (p >3 ))
+ //   .average();
     .sum();
+    //                       double sum2 = Arrays.stream(NUMBERS);
+  //                                 .average();
+//	.distinct();
     
     Stream<String> stream = Stream.of("java 8 ", "leveluplunch.com","    some messy string    ",
             "examples", "exercises");
@@ -101,7 +106,8 @@ public void stream_from_file() throws IOException {
 		System.out.println("Mike");
 		System.out.println(joined);
 		System.out.println(numbers);
-		System.out.println(sum);		
+		System.out.println(sum);	
+	//	System.out.println(sum2);	
 	}
 }
 
