222b6e38e558ebaaba10fb38cbf509c4f72b624c
Michael J Hewitt <matroxmike@icloud.com>
2015-08-26 22:16:43 +1000

    added imports 2

--- streamffile.java
+++ streamffile.java
@@ -14,6 +14,47 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;	
+import java.util.stream.Collectors.*; 
+
+
+import java.util.stream.Stream;	
+import java.util.stream.Collectors.*; 
+import java.util.List;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Date;
+
+
+
+
+
 
 // 26.08.2015 20:30:52
 
@@ -32,19 +73,22 @@ public static void stream_from_values() {
 
 public void stream_from_file() throws IOException {
 
-    long uniqueWords = java.nio.file.Files
-            .lines(Paths.get("word-occurrences-in-file.txt")    //,
+ //   long uniqueWords = java.nio.file.Files
+       //     .lines(Paths.get("word-occurrences-in-file.txt")    //,
          //           Charset.defaultCharset())
-            .flatMap(line -> Arrays.stream(line.split(" ."))).distinct()
-            .count());
+     //       .flatMap(line -> Arrays.stream(line.split(" ."))).distinct()
+        //    .count());
 
-    assertEquals(80, uniqueWords);
+  //  assertEquals(80, uniqueWords);
 }
 
 	public static void main (String args[]) {
 		    int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
 
-    int sum = Arrays.stream(numbers).sum();
+    int sum = Arrays.stream(numbers)
+   // .unique()
+    .filter ( p -> (p >3 ))
+    .sum();
     
     Stream<String> stream = Stream.of("java 8 ", "leveluplunch.com","    some messy string    ",
             "examples", "exercises");
