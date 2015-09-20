4f66aa1b69f71be99e53773fc6bb3b440f010475
Mike Hewitt <matroxmike@icloud.com>
2015-09-20 16:03:01 +1000

    limited infinite loop on stream

--- StreamBuilder3.java
+++ StreamBuilder3.java
@@ -17,7 +17,20 @@ import java.util.Date;
 
 public class StreamBuilder3 {
      public static void main(String[] args){
-         Stream<Date> stream = Stream.generate(() -> { return new Date();});
+         Stream<Date> stream = Stream.generate(() -> { return new Date();}
+         )
+         .limit(9)		// added 20.09.2015 14:21:07
+						// looked for ; and () after generate and added .limit(n)
+						// note that here the generate() contains actions to create new data 
+						//
+						// now that we have tamed this example we can doe some useful tests - 
+						// this e.g. returns a stream of the same date 
+						// something useful for me would be to create the forthcoming dates and test for some feature e.g. second Thursday of the month
+						// see Dates1.java
+						
+         ;
+         
+         //  limit
          stream.forEach(p -> System.out.println(p));
      }
 }
