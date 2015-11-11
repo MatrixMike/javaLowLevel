118983d7d8855f27559cb8daa777af11c097d4d2
MatrixMike <matroxmike@icloud.com>
2015-11-11 21:49:50 +1100

    removed unnecessary comments 2

--- Dates1.java
+++ Dates1.java
@@ -26,18 +26,21 @@ public class Dates1 {
 	Date startDate2 = new GregorianCalendar(2015, 10,  1, 9, 00).getTime();
 	Date startDate3 = new GregorianCalendar(2015,  8,  1, 9, 00).getTime();
 	Date startDate4 = new GregorianCalendar(1955,  10,  1, 9, 00).getTime();
+	Date startDate5 = new GregorianCalendar(1947,  10,  1, 9, 00).getTime();
 	Date endDate1   = new Date();
 
    long diff1 = endDate1.getTime() - startDate1.getTime();
    long diff2 =   startDate2.getTime() - endDate1.getTime();
    long diff3 =   startDate3.getTime() - endDate1.getTime();
    long diff4 =   startDate4.getTime() - endDate1.getTime();
+   long diff5 =   startDate5.getTime() - endDate1.getTime();
       
 	System.out.println("  Difference between " + endDate1);
 	System.out.println("  and " + startDate1 + " is "  + (diff1 / (1000L*60L*60L*24L)) + " days.");
 	System.out.println("  and " + startDate2 + " is "  + (diff2 / (1000L*60L*60L*24L)) + " days.");
 	System.out.println("  and " + startDate3 + " is "  + (diff3 / (1000L*60L*60L*24L)) + " days. LUV AGM");
 	System.out.println("  and " + startDate4 + " is "  + (diff4 / (1000L*60L*60L*24L)) + " days. Mike's Life");
+	System.out.println("  and " + startDate5 + " is "  + (diff5 / (1000L*60L*60L*24L)) + " days. Phyllis' Life");
 	System.out.println();
    }
 	  private static void Adddates() {
