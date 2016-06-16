a88c5343a9e5b3ac2ce0f02dddec7f353e8696c3
Mike Hewitt <matroxmike@icloud.com>
2016-06-16 19:53:05 +1000

    changed linux filepath for input data to windows file path
    hand copied the updated file from Netbeans to git repo

--- TabGen.java
+++ TabGen.java
@@ -1,9 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tabgen;
+/**
+ *
+ * @author MikeW7
+ */
+/**
+ * @param args the command line arguments
+ */
 // import java.lang.*;
 // import java.util.*;
 //import java.lang.Object;
 // import java.lang.object;
 // 05 October 2011
-
+// mod in Netbeans 16/06/2016
 // TabGen.java:28: error: unreported exception FileNotFoundException; must be caught or declared to be thrown
 import java.io.File;
 import java.text.Format;
@@ -12,7 +25,6 @@ import java.util.Scanner;
 import java.io.FileNotFoundException;
 
 public class TabGen {
-
 // on Tony IBM testing Java 7 ?
     public static void header() {
         System.out.println("<head>");
@@ -24,18 +36,21 @@ public class TabGen {
         String name;               // Declare a variable to hold the name.
         String names[];
 //	Scanner in = new Scanner(System.in);
-        Scanner scanner = new Scanner(new File("/home/mikeh/javaLowLevel/TabGenIn.txt"));
+        //       Scanner scanner = new Scanner(new File("/home/mikeh/javaLowLevel/TabGenIn.txt"));
+        Scanner scanner = new Scanner(new File("C:\\Users\\MikeW7\\Documents\\NetBeansProjects\\TabGen\\testInput\\TabGenIn.txt"));
+        // C:\Users\MikeW7\Documents\GitHub\javaLowLevel
+        // C:\Users\MikeW7\Documents\NetBeansProjects\TabGen\testInput\TabGenIn.txt
 // DateFormat df = DateFormat.getDateInstance();
         System.out.println("<html>");    //  header
         header();
         System.out.println("<body>");
         System.out.println("<H1 ALIGN=CENTER>My PIC projects</H1>");
         int n = 1;
-        String one = "http://www.electroteach.com/pics/TabGen.html";
+        String one = "https://electroteach.com/pics/TabGen.html";
         String two = "Explanation here</A>";
 
         System.out.println("<TABLE BORDER ALIGN=CENTER>");
- // System.out.println( "<TABLE>") ;
+        // System.out.println( "<TABLE>") ;
 
         for (int rows = 1; rows < 10; rows = rows + 1) {
             System.out.print("<TR>");
