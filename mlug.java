97352dfbeeaa61ea987404347ca84400734ba8a9
MatrixMike <matroxmike@icloud.com>
2016-08-04 22:08:22 +1000

    a tested ? version that produces dates as checked by 'cal 2016' and 'cal 2017'

--- mlug.java
+++ mlug.java
@@ -35,10 +35,24 @@ static void weekCheck(Calendar C, SimpleDateFormat S){
 		String testStr; 
 //		    Calendar now = Calendar.getInstance();
 	 if (  ( (C.get(Calendar.DAY_OF_WEEK_IN_MONTH)==4) && (C.get(Calendar.DAY_OF_WEEK)==2) )){
-		System.out.println("found something");
-		testStr = S.format(C.getTime());
-		System.out.println(testStr);
-		int month = C.get(Calendar.MONTH); 
+	//	System.out.println("found something");
+	
+	//x	testStr = S.format(C.getTime());
+	//x	System.out.println(testStr);
+		
+		int monthA = C.get(Calendar.MONTH); 
+		C.add(C.DAY_OF_YEAR, +7);	
+		int monthB = C.get(Calendar.MONTH); 
+		if (monthA == monthB) {
+		//	System.out.println("same");
+		//	System.out.println("<same> "+S.format(C.getTime()));  // "same"
+						System.out.println(S.format(C.getTime()));  // "same"
+		}
+		else { // print derived from monthA
+					C.add(C.DAY_OF_YEAR, -7);	
+				//	System.out.println("<Not same> "+S.format(C.getTime()));	// "Not same"		
+					System.out.println(S.format(C.getTime()));
+		}
 		/*
 		get the month of current date -> Ma
 		* add 7 days to date
@@ -46,7 +60,8 @@ static void weekCheck(Calendar C, SimpleDateFormat S){
 		* check if they are same
 		
 		*/
-    if(month == C.AUGUST){
+		/*
+    if(monthA == C.AUGUST){
       System.out.println("AUGUST");
       C.add(C.DAY_OF_YEAR, +7);			// look ahead for the following month
       	     System.out.println("B "+S.format(C.getTime()));	
@@ -56,6 +71,7 @@ static void weekCheck(Calendar C, SimpleDateFormat S){
 	     System.out.println("SEPTEMBER");	  
       }
 	 }
+	 * */
 	 // need to get the month and save for a test
     }
 
