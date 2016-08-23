92295a4f3ac51e334988948f992e87e845e8837a
MatrixMike <matroxmike@icloud.com>
2016-08-23 22:31:42 +1000

    more refactoring - try importing into BlueJ

--- generateStream.java
+++ generateStream.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package generatestream;
+//package generatestream;
 /**
  * @version 1.1
  * @author Mike Hewitt
