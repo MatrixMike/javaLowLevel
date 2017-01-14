8b0fe760d3790d97ed2485e69cf90df280da2920
Mike J H <matrixmike@icloud.com>
2017-01-15 04:59:56 +1100

    after processing with bluej

--- CBfreq.java
+++ CBfreq.java
@@ -19,57 +19,57 @@
  */
 
 public class CBfreq {
-	
-	public static void main (String args[]) {
-		int ch = 1;
-		
-		String palindrome = "Dot saw I was Tod";
-         
+
+    public static void main (String args[]) {
+        int ch = 1;
+
+        String palindrome = "Dot saw I was Tod";
+
         //StringBuilder sb = new StringBuilder(palindrome);
 
-		//sb.append("Greetings");
-		//System.out.println(" length = " + sb.length() + " "+  sb.reverse() + sb);
-		System.out.println("\nStarting\n");
-		for (int x = 156050; x < 157350+1; x=x+50) {
-			/* StringBuilder sb = new StringBuilder();
-			sb.append("\nM ch ");
-			sb.append(ch);
-			sb.append(" F = ");
-			sb.append(x/1000);
-			sb.append(".");
-			sb.append(x%1000);
-			sb.append("MHz");
-			System.out.println(sb);
-			// System.out.println("\nM ch "+ ch + " F = " + x/1000 + "." + x%1000 +"MHz");
-			// now reset to blank 
-			* */
-			System.out.println(strCreate.make1(ch,x));
-			ch += 1;
-		}
-			System.out.println("\n===M ch 28 F = 157.5MHz===");	
-			
-		ch = 60;
-		for (int x = 156025; x < 157425+1; x=x+50) {
-			System.out.println("\nM ch "+ ch + " F = " + x/1000 + "." + x%1000 +"MHz");
-			ch = ch + 1;
-		}
-	}
-/*	static class strCreate  {
-		static String make1 (int chn, int f ){
-			String message1 = "";
-			//int f = 156025;
-			StringBuilder sb1 = new StringBuilder(message1);
-			sb1.append("M ch ");
-			sb1.append(chn);
-			sb1.append(" F = ");
-			sb1.append(f/1000);
-			sb1.append(".");
-			sb1.append(f%1000);
-			sb1.append("MHz\n");
-			//return message1; // return sb1;   work done to create sb1  
-			return sb1.toString();
-		}
-	}
-	*/
+        //sb.append("Greetings");
+        //System.out.println(" length = " + sb.length() + " "+  sb.reverse() + sb);
+        System.out.println("\nStarting\n");
+        for (int x = 156050; x < 157350+1; x=x+50) {
+            /* StringBuilder sb = new StringBuilder();
+            sb.append("\nM ch ");
+            sb.append(ch);
+            sb.append(" F = ");
+            sb.append(x/1000);
+            sb.append(".");
+            sb.append(x%1000);
+            sb.append("MHz");
+            System.out.println(sb);
+            // System.out.println("\nM ch "+ ch + " F = " + x/1000 + "." + x%1000 +"MHz");
+            // now reset to blank 
+             * */
+            System.out.println(strCreate.make1(ch,x));
+            ch += 1;
+        }
+        System.out.println("\n===M ch 28 F = 157.5MHz===");	
+
+        ch = 60;
+        for (int x = 156025; x < 157425+1; x=x+50) {
+            System.out.println("\nM ch "+ ch + " F = " + x/1000 + "." + x%1000 +"MHz");
+            ch = ch + 1;
+        }
+    }
+    /*	static class strCreate  {
+    static String make1 (int chn, int f ){
+    String message1 = "";
+    //int f = 156025;
+    StringBuilder sb1 = new StringBuilder(message1);
+    sb1.append("M ch ");
+    sb1.append(chn);
+    sb1.append(" F = ");
+    sb1.append(f/1000);
+    sb1.append(".");
+    sb1.append(f%1000);
+    sb1.append("MHz\n");
+    //return message1; // return sb1;   work done to create sb1  
+    return sb1.toString();
+    }
+    }
+     */
 }
 
--- strCreate.java
+++ strCreate.java
@@ -1,16 +1,16 @@
-	public  class strCreate  extends CBfreq{
-		static   String make1 (int chn, int f ){
-			String message1 = "";
-			//int f = 156025;
-			StringBuilder sb1 = new StringBuilder(message1);
-			sb1.append("M ch ");
-			sb1.append(chn);
-			sb1.append(" F = ");
-			sb1.append(f/1000);
-			sb1.append(".");
-			sb1.append(f%1000);
-			sb1.append("MHz\n");
-			//return message1; // return sb1;   work done to create sb1  
-			return sb1.toString();
-		}
-	}
+public  class strCreate  extends CBfreq{
+    static   String make1 (int chn, int f ){
+        String message1 = "";
+        //int f = 156025;
+        StringBuilder sb1 = new StringBuilder(message1);
+        sb1.append("M ch ");
+        sb1.append(chn);
+        sb1.append(" F = ");
+        sb1.append(f/1000);
+        sb1.append(".");
+        sb1.append(f%1000);
+        sb1.append("MHz\n");
+        //return message1; // return sb1;   work done to create sb1  
+        return sb1.toString();
+    }
+}
