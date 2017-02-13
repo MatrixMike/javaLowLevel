25ec4753305b7b06ad6f7ca866b61288d1a12ea7
MatrixMike <matroxmike@icloud.com>
2017-02-13 19:16:20 +1100

    recursive examples

---
+++ LongestCommonSubsequence.java
@@ -0,0 +1,62 @@
+/*
+ * LongestCommonSubsequence.java
+ * 
+ * Copyright 2017 Michael Hewitt <mikeh@electroteach.com>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+public class LongestCommonSubsequence {
+
+    // Compute length of LCS for all subproblems.
+    public static String lcs(String x, String y) {
+        int m = x.length(), n = y.length();
+        int[][] opt = new int[m+1][n+1];
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
+                if (x.charAt(i) == y.charAt(j)) {
+                    opt[i][j] = opt[i+1][j+1] + 1;
+                }
+                else {
+                    opt[i][j] = Math.max(opt[i+1][j], opt[i][j+1]);
+                }
+            }
+        }
+
+        // Recover LCS itself.
+        String lcs = "";
+        int i = 0, j = 0;
+        while (i < m && j < n) {
+            if (x.charAt(i) == y.charAt(j)) {
+                lcs += x.charAt(i);
+                i++;
+                j++;
+            }
+            else if (opt[i+1][j] >= opt[i][j+1]) i++;
+            else                                 j++;
+        }
+        return lcs;
+    }
+
+    public static void main(String[] args) {
+        String lcs = lcs(args[0], args[1]);
+        System.out.println(lcs);
+    }
+}
+
