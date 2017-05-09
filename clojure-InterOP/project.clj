cf86199716bc28df1cf760757fd09fb04961b019
Mike Hewitt <matroxmike@icloud.com>
2017-05-09 16:56:19 +1000

    compiles but not exec

--- clojure-InterOP/project.clj
+++ clojure-InterOP/project.clj
@@ -4,5 +4,7 @@
   
   
     :main clojure-InterOP.core
+    :aot 
+    :all
   )
   
--- clojure-InterOP/src/clojure_InterOP/core.clj
+++ clojure-InterOP/src/clojure_InterOP/core.clj
@@ -1,7 +1,8 @@
 (ns clojure-InterOP.core
-(:import utils.demo)
+(:import utils.demo)  ;; java source file needs to be compiled to a class file
        	(:gen-class ))
 
 (defn -main [& args]
 (println "clj-test")
+(println (utils.demo 4      ))  ;; compiles but error on execution
 )
