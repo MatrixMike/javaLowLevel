b55e57bb542a897939ff9f32cdf0abde002b33fb
Mike on Evo N410c <matroxmike@icloud.com>
2017-05-11 15:23:46 +1000

    double and multiple inputs

--- clojure-InterOP/src/utils/demo.java
+++ clojure-InterOP/src/utils/demo.java
@@ -4,4 +4,12 @@ public class demo {
 	public static int twice (int a){
 	return a+a;
 	}
+
+	public static double half (int a){
+	return a/2;
+	}
+	
+	public static double halves (int a, int b){
+	return a/2 + b/2;
+	}
 }
--- page58/utils/demo.java
+++ page58/utils/demo.java
@@ -4,4 +4,10 @@ public class demo {
 	public static int twice (int a){
 	return a+a;
 	}
+
+
+
+	public static double half (int a){
+	return a/2;
+	}
 }
