335c86d6545d305a216a6c2fb3562767e8c6890d
Mike Hewitt <matroxmike@icloud.com>
2017-05-27 21:11:06 +1000

    working version of fizz buzz
    missing numbers in sequence should be replaced by fizz or buzz

--- PredicateTest1.java
+++ PredicateTest1.java
@@ -1,29 +1,26 @@
 
-
 import java.util.stream.Stream;
-import java.util.stream.Collectors.*;
-import java.util.List;
-import java.util.Arrays;
+//import java.util.stream.Collectors.*;
+//import java.util.List;
+//import java.util.Arrays;
 import java.util.function.Predicate;
-
+// 27 May 2017
 
 public class PredicateTest1 {
 
-		public static void stream_from_function() {
+	public static void stream_from_function() {
+		Predicate <Integer> fizz = 	i -> (i % 3 != 0 );
+		Predicate <Integer> buzz = 	i -> (i % 5 != 0 );
 		Stream.iterate(0, n -> n + 1)
-		.limit(15)
-	//	.skip(2)
-	//	.filter(w -> (w % 5) == 0)
+		.limit(55)
+		.filter(fizz)
+		.filter(buzz)
 	//	.forEach(System.out::println);
-				.forEach(System.out::print);
-	}
+				.forEach(System.out::println);
+}
 
 	public static void main (String args[]) {
 		Predicate <String> gr99 = 	s -> (s.length() > 99);
-		Predicate <Integer> fizz = 	i -> (i / 3 == 0 );
-		Predicate <Integer> buzz = 	i -> (i / 5 == 0 );
-         stream_from_function();
-
-	}
-	}
+        stream_from_function();
+} }
 
