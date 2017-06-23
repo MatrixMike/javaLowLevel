67003f542ea38562526c553363c79c7b767c8636
Mike J Hewitt <mikeh@electroteach.com>
2017-06-23 23:42:18 +1000

    2

--- Streams1.java
+++ Streams1.java
@@ -29,11 +29,22 @@ myList
     .map(String::toUpperCase)
     .sorted()
     .forEach(System.out::println);
-
+//++++
 Stream.of("d2", "a2", "b1", "b3", "c")
     .filter(s -> {
         System.out.println("filter: " + s);
         return true;
+    })
+    .forEach(s -> System.out.println("forEach: " + s));
+//++++	
+	Stream.of("d2", "a2", "b1", "b3", "c")
+    .filter(s -> {
+        System.out.println("filter: " + s);
+        return s.startsWith("a");
+    })
+    .map(s -> {
+        System.out.println("map: " + s);
+        return s.toUpperCase();
     })
     .forEach(s -> System.out.println("forEach: " + s));
 	
