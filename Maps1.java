ee73547423d4cede008eead1564ac2835ad524e6
Mike J Hewitt <mikeh@electroteach.com>
2017-06-26 20:15:39 +1000

    maps and lambdas2

--- Maps1.java
+++ Maps1.java
@@ -12,7 +12,7 @@ public class Maps1 {
 	public static void main (String[] args) {
 		Map<Integer, String> map = new HashMap<>();
 
-	for (int i = 0; i < 10; i++) {
+	for (int i = 10; i > 0; i--) {
 		map.putIfAbsent(i, "val" + i);
 	}
 	for (int i = 5; i < 15; i++) {
