d92a786e486b603f9ee0f8b50113727127f11ba6
Mike J Hewitt <mikeh@electroteach.com>
2017-11-02 23:28:42 +1100

    working lumo

--- lumo/lumo.java
+++ lumo/lumo.java
@@ -1,9 +1,8 @@
 /*
- * Friday13.java
  * 
  * Copyright 2015 Michael Hewitt <mikeh@electroteach.com>
- * 11 November 2015
- * mod for mlug 04.08.2016 11:10:30   mlug.java
+ * 02.11.2017
+ *
  * http://www.java2s.com/Tutorials/Java/java.util/Calendar/Java_Calendar_get_int_field_.htm
  */
 /*
@@ -22,14 +21,26 @@ public class lumo {
      * @version (1.1)
      */
     public static void main (String args[]) {
+        Double LumoInstallments = 75.00;
+        Integer Installments = 14;
         Calendar cldr = Calendar.getInstance();  // change 'M' - MM -> 2 digits; MMM -> 3 letters
         SimpleDateFormat dateformatter =   new SimpleDateFormat("dd-MMM-yyyy");  // ("dd-MM-yyyy E 'at' hh:mm:ss a zzz");
 
-        for (int n= - cldr.get(Calendar.DAY_OF_MONTH); n<daysAhead;  n=n+1) {  
-            {  // add here a check that month is Jan thru Nov -> so don't do weekCheck for December 
-                weekCheck(cldr, dateformatter);
+        cldr.set(Calendar.YEAR, 2017);			// set the year (start date for repayment) 11/09/2017
+        cldr.set(Calendar.MONTH, 8);			// set the month September
+        cldr.set(Calendar.DAY_OF_MONTH, 11);	// set the date
+        System.out.printf("Lumo Bill payment scheme%n%n");
+        for (int n= 1; n<Installments;  n=n+1) {  
+            {
+                balanceCheck(cldr, dateformatter);
+                System.out.printf("Installment "+ n + "%n");
+                if (LumoFortnight(cldr, dateformatter)) {
+
+                    System.out.println("Installments" + Installments);
+                    //				                    balanceCheck(cldr, dateformatter));
+                }
             }
-            cldr.add(Calendar.DAY_OF_YEAR, +1);  //  examine why cldr and Calendar 
+            cldr.add(Calendar.DAY_OF_YEAR, +14);  //  examine why cldr and Calendar 
         }
     }
 
@@ -40,6 +51,33 @@ public class lumo {
      * @param C supply instance(?) of Calendar
      * @param S supply previously organised display format
      */
+    public static void balanceCheck(Calendar C, SimpleDateFormat S){
+        String testStr; 
+        //		Calendar now = Calendar.getInstance();
+
+        //	System.out.println("found something");
+        System.out.print(" "+ S.format(C.getTime())+" ");
+    }    
+
+    /**
+     * consider a function to return bool if Rent date and another one to return bool if Newstart payment
+     * 
+     */
+    public static boolean LumoFortnight(Calendar C, SimpleDateFormat S){	 
+        return (((
+                    (C.get(Calendar.DAY_OF_YEAR)) - 50) % 14) == 0 )
+        ;
+        //&& (C.get(Calendar.DAY_OF_WEEK)==2) ))	 
+    }
+
+    /**
+     * weekCheck()
+     * @author  (Mike Hewitt)
+     * @version (1.1)
+     * @param C supply instance(?) of Calendar
+     * @param S supply previously organised display format
+     */
+
     public static void weekCheck(Calendar C, SimpleDateFormat S){
         String testStr; 
         //		    Calendar now = Calendar.getInstance();
@@ -62,27 +100,7 @@ public class lumo {
                 //	System.out.println("<Not same> "+S.format(C.getTime()));	// "Not same"		
                 System.out.println(S.format(C.getTime()));
             }
-            /*
-            get the month of current date -> Ma
-             * add 7 days to date
-             * get the month of the new date -> Mb
-             * check if they are same
 
-             */
-            /*
-            if(monthA == C.AUGUST){
-            System.out.println("AUGUST");
-            C.add(C.DAY_OF_YEAR, +7);			// look ahead for the following month
-            System.out.println("B "+S.format(C.getTime()));	
-            //   int month2 = C.get(C.MONTH);
-            C.add(C.DAY_OF_YEAR, +7);	
-            if(C.get(C.MONTH) == C.SEPTEMBER){
-            System.out.println("SEPTEMBER");	  
-            }
-            }
-             * */
-            // need to get the month and save for a test
         }
-
     }
 }
