b53c8e510ac0ced63f4a6dd2db2c030f7ccfaf5b
Mike Hewitt <matroxmike@icloud.com>
2017-11-23 11:52:37 +1100

    mini batch file for generating financial help

--- centrelink/centrelinkBat.sh
+++ centrelink/centrelinkBat.sh
@@ -1,3 +1,5 @@
-javac -cp centrelink/ centrelink/Centrelink.java 
- 
-java -cp centrelink/ Centrelink 
+javac  -cp centrelink/    centrelink/Centrelink.java  
+java   -cp centrelink/    Centrelink 
+
+javac  -cp DateExamples/  DateExamples/lumo2.java 
+java   -cp DateExamples/  lumo2
