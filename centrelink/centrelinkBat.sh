956655f556ecc46ddda2cccb5ad85be6acefd91c
Mike J Hewitt <mikeh@electroteach.com>
2018-01-23 20:15:39 +1100

    grouping examples together

--- centrelink/centrelinkBat.sh
+++ centrelink/centrelinkBat.sh
@@ -3,3 +3,7 @@ java   -cp centrelink/    Centrelink
 
 javac  -cp DateExamples/  DateExamples/lumo2.java 
 java   -cp DateExamples/  lumo2
+
+javac  -cp DateExamples/ DateExamples/WOWdates.java
+java   -cp DateExamples/ WOWdates
+
