8d6ede0eea6cb88522cc32f279ce3c0b4a91d10c
Mike <matroxmike@icloud.com>
2020-02-20 14:53:40 +1100

    experiments re-ify

---
+++ Playlist.java
@@ -0,0 +1,95 @@
+/*
+https://www.dreamincode.net/forums/topic/143407-writing-a-getlist-method/
+
+
+*/
+//  private Song [ ] list;
+/*
+  public Playlist ()
+  {
+	list = new Song [0];
+  }	
+  */
+public list = new Song [0];
+public class Playlist
+{
+
+	//return an array of songs instead of a single String
+/*	public Song[] getList()
+	{list = new Song [0];
+	   Song[] songs = new Song[list.length];
+	   for (int i = 0; i < list.length; i++) 
+			   songs[i] = list[i];
+		return songs;
+	}
+	
+*/
+  
+	 public String getList()
+	{
+		   String[] songs = new String[list.length];
+	   for (int i = 0; i < list.length; i++) 
+			   songs += list[i];
+		   return songs;
+	}
+
+	
+	public void add(Song song)
+	{
+		if (songToAdd.inList())
+		{
+			Song[] localArray = new Song[playList.length+1];
+			int localIndex = 0;
+			 for (int i = 0; i > playList.length; i++)
+			 {
+				if (!songToAdd.eqauls(playList[i]))
+				 {
+					 localArray[localIndex] = playList[i];				  
+					 localIndex++;
+									 
+			   playList = localArray;	
+			   System.out.println(songToAdd + " was added to the play list.");
+				}
+			}
+		}
+
+	}
+	 
+	public void remove(Song song)
+	{ 
+		if (songToRemove.inList()) 
+		{
+			Song[] localArray = new Song[playList.length-1];
+			int localIndex = 0;
+
+		   for (int i = 0; i < playList.length; i++)
+			{
+				if (!songToRemove.eqauls(playList[i]))
+				{
+					localArray[localIndex] = playList[i];				  
+					localIndex++;
+				}
+
+			playList = localArray;
+			System.out.println(songToRemove + " was removed from the playlist.");
+		   }
+		   
+		}
+	}
+	
+	public boolean inList(Song Song)
+	{
+		x = false;
+		if (this.Song == Song)
+		{
+			x = true;
+		}
+		 else
+		 {
+			return false;
+		 } 
+		}
+}
+
+
+
--- Reified1.java
+++ Reified1.java
@@ -2,35 +2,49 @@
  * Reified1.java
  * https://www.quora.com/What-is-reification-in-Java
  * https://dev.to/cjbrooks12/kotlin-reified-generics-explained-3mie
- * https://dev.to/cjbrooks12/kotlin-reified-generics-explained-3mie
+ * https://www.geeksforgeeks.org/data-types-in-java/
+ * https://www.dreamincode.net/forums/topic/143407-writing-a-getlist-method/
  */
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 
+
+/*
+The two categories of types are primitive types and reference types. 
+* There is also a null type. 
+* The primitive types are boolean, byte, short, int, long, char, float, and double.
+*  The reference types are classes, interfaces, and arrays.
+*/
 public class Reified1 {
 	
-	public static void main (String[] args) {
+public static void main (String[] args) {
 
  //Allocates an array of type String, so it is reified
 String[] aStringArray = new String[10];
 Integer[] mjhArray = new Integer[12];
-/* class JavaClass {
-    <T> List<T> getList() { ... }
+Boolean[] mjhArrayB = new Boolean[12];
+ArrayList<String> listm = new ArrayList<>();	
+
+
+/*
+ class JavaClass {
+    <T> List<T> getList() { return listm; }
 }
 */
 
-// List<String> list = new JavaClass().getList<String>();
+
+
 
 //Allocates a list with no type, Java does not reify generic types
 
 List<String> aStringList = new ArrayList<String>();
-
+List<Integer> anIntegerList = new ArrayList<Integer>();
 }
 //	List<String> list = new JavaClass().getList<String>();		
 	}
 
-/* class JavaClass {
+ class JavaClass {
     <T> T getItemFromMap(Class<T> itemClass, String key) { 
         Object item = _map.get(key); // this is OK
         if(itemClass.isAssignableFrom(item.getClass())) { // this works
@@ -41,7 +55,7 @@ List<String> aStringList = new ArrayList<String>();
         }
     }
 }
-*/
+ // List<String> list = new JavaClass().getList<String>();
 
 /* inline fun <reified T> getItemFromMap(String key) { 
     val item = _map.get(key)
