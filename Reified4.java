7d21a4fdf7e6f2901a6c6a51ecc3b8e83c91ab84
Mike <matroxmike@icloud.com>
2020-02-20 16:56:21 +1100

    a working version

--- Reified4.java
+++ Reified4.java
@@ -1,11 +1,30 @@
+
+/*
+20.02.2020 16:55:18
+
+
+*/
+
 import java.util.List;
 import java.util.ArrayList;
 
-class Main {
+public class Reified4 {
   public static void main(String[] args) {
     List<String> list = new ArrayList<String>();
     list.add("Hi");
+    list.add("Mike");
     String x = list.get(0);
+    System.out.println(x);
+     x = list.get(1);
     System.out.println(x);
   }
 }
+
+
+/*
+public class Reified1 {
+	
+public static void main (String[] args) {
+
+
+*/
