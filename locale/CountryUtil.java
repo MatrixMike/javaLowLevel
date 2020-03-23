a82016ae1e8a8c34c873b261f35164cd86413d02
Mike <matroxmike@icloud.com>
2020-03-23 22:35:33 +1100

    more locale Country class

--- locale/CountryUtil.java
+++ locale/CountryUtil.java
@@ -22,7 +22,55 @@
  */
 
 import java.util.*;
+class Country {
 
+    private String countryCode;
+
+    private String name;
+
+    public Country(String countryCode, String name)
+
+    {
+
+        this.countryCode = countryCode;
+
+        this.name = name;
+
+    }
+
+    public String getCountryCode() {
+
+        return countryCode;
+
+    }
+
+    public void setCountryCode(String countryCode) {
+
+        this.countryCode = countryCode;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public String toString()
+
+    {
+
+        return countryCode + ", " + name;
+
+    }
+
+}
 public class CountryUtil {
 
   public static List<Country> getCountries(final Locale inLocale) {
--- locale/ListCountries.java
+++ locale/ListCountries.java
@@ -15,7 +15,55 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 import java.util.Locale;
+class Country {
 
+    private String countryCode;
+
+    private String name;
+
+    public Country(String countryCode, String name)
+
+    {
+
+        this.countryCode = countryCode;
+
+        this.name = name;
+
+    }
+
+    public String getCountryCode() {
+
+        return countryCode;
+
+    }
+
+    public void setCountryCode(String countryCode) {
+
+        this.countryCode = countryCode;
+
+    }
+
+    public String getName() {
+
+        return name;
+
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+
+    }
+
+    public String toString()
+
+    {
+
+        return countryCode + ", " + name;
+
+    }
+
+}
 public class ListCountries {
 
     public static void main(String... args){
