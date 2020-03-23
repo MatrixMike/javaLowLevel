2478d9b3c0b9b979e8e31429d2f520fb1e2ee8c0
MatroxMike on Optiplex <matroxmike@icloud.com>
2020-03-23 13:49:59 +1100

    between java 11 and java 8

---
+++ locale/ListCountries.java
@@ -0,0 +1,49 @@
+/*
+ * country2.java
+ * 
+ * Copyright 2020 Mike <mikeh@mikeh-OptiPlex-745>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+// https://memorynotfound.com/java-display-list-countries-using-locale-getisocountries/
+ * 
+ */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import java.util.Locale;
+
+public class ListCountries {
+
+    public static void main(String... args){
+
+        // Create a collection of all available countries
+//        List<Country> countries = new ArrayList<Country>();
+
+        // Map ISO countries to custom country object
+        String[] countryCodes = Locale.getISOCountries();
+        for (String countryCode : countryCodes){
+
+            Locale locale = new Locale("", countryCode);
+            String iso = locale.getISO3Country();
+            String code = locale.getCountry();
+            String name = locale.getDisplayCountry();
+
+            countries.add(new Country(iso, code, name));
+        }
+
+        // Sort countries
+        Collections.sort(countries);
+
+        // Loop over collection of countries and print to console
+        countries.forEach((System.out::println));
+
+        // print total number of countries
+        System.out.println("found: " + countries.size() + " countries");
+
+    }
+
+}
---
+++ locale/MainLocaleMap3.java
@@ -0,0 +1,86 @@
+/*
+ * MainLocaleMap.java
+ * 
+ * Copyright 2016 Michael Hewitt <mikeh@electroteach.com>
+ * 
+ * 
+ */
+//package localeDemo;
+import java.text.NumberFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+//import java.util.*;
+import java.util.function.Predicate;
+//-import java.util.List;
+//-import java.util.ArrayList;
+public class MainLocaleMap3 {
+
+  public static void main(String[] args) {
+//-        List<Country> countries = new ArrayList<Country>();
+		// filter out 10,12,43
+			Predicate <Integer> badPrint = bp -> !((bp == 10) | (bp == 12) | (bp == 43)) ;
+    Calendar now = Calendar.getInstance();
+// create an object of locale class 
+    Locale[] arrayN    =       Locale.getAvailableLocales();
+    Locale[] localesN = NumberFormat.getAvailableLocales();
+    
+    Locale locale = Locale.getDefault();
+         locale = Locale.FRANCE;
+
+	//String here = Locale.getDisplayCountry();
+	System.out.println("length= "+ localesN.length);
+	String viet = "English";
+	System.out.println(viet);
+	for (int i = 0; i < localesN.length; i ++) {  
+		// omit 58,62,67,83,400,441,475,493,511,595,611,614,619,691,747,746
+		locale = arrayN[i];
+
+    // call the getDisplayNames method
+    if (i !=57 & i != 67 & i!= 83 & i!=62 & i != 400 & i!=437 & i!=441 & 
+    i!=475 & i!=493 & i!=511 & i!=595 & i!=611 & i!=614 & i!=619 )
+    {
+		// (i != 53)
+// https://stackoverflow.com/questions/13612710/java-util-missingresourceexception-couldnt-find-3-letter-country-code-for-cs
+         Map<String, Integer> representations1 = now.getDisplayNames(
+               Calendar.DAY_OF_WEEK, Calendar.LONG, locale);
+
+         Map<String, Integer> representations2 = now.getDisplayNames(
+               Calendar.MONTH, Calendar.LONG, locale);
+    
+         NavigableMap<String, Integer> navMap1 = new TreeMap<String, Integer>(   // TreeMap
+             representations1);
+         NavigableMap<String, Integer> navMap2 = new TreeMap<String, Integer>(   // TreeMap
+             representations2);
+         // print the results
+         //https://docs.oracle.com/javase/tutorial/java/data/numberformat.html
+         System.out.printf("<%3d> ",i);  // change to field of 3 for neater grep sorting
+  //       System.out.print(localesN[i].getDisplayName());
+         System.out.print("{"+arrayN[i].getISO3Country()+ "} ");
+         System.out.print("<"+arrayN[i].getISO3Language()+ "> ");
+         System.out.println("+"+locale.getCountry()+"+");
+			System.out.println("*"+localesN[i].getDisplayName().toString()+"*");         
+         if (localesN[i].getDisplayName().toString().equals(viet)  ) //arrayN[i].getDisplayLanguage())
+			System.out.println("Tsame");  // investigate the comparison facilities - // extra as same found in samedi
+
+          System.out.println( ">>> "+ localesN[i].getDisplayName().toString()  );
+   //mx      System.out.print(arrayN[i].getDisplayLanguage());
+//         System.out.print("{"+arrayN[i].getISO3Country()+ "} ");
+//         System.out.print("/"+localesN[i].getDisplayLanguage()+"/");
+//#3         System.out.printf(" %s %s %n", navMap1, navMap2);  // sorted alphabetically
+//         System.out.printf("%n%s %n", navMap2);  // sorted alphabetically
+  //  System.out.printf("%n%s %n", navMap);  // sorted alphabetically
+  
+  // Sentence construction : the land is between the () but not yet understood how to extract this...
+		System.out.println("In "+localesN[i].getDisplayName() +" they speak " +   localesN[i].getDisplayLanguage()  +"."          );
+		System.out.println();
+    /* Whole list:
+{Friday=6, Monday=2, Saturday=7, Sunday=1, Thursday=5, Tuesday=3, Wednesday=4}
+ */}
+}
+
+  }
+}
+//08.03.2020 10:42:44  add checks : if country == language or country not equal to language  : 
