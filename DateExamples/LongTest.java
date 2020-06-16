97410193dac49613129dc9b118ac8d1a52a013d3
Mike <matroxmike@icloud.com>
2020-06-16 22:39:30 +1000

    Integers .equals()

--- DateExamples/LongTest.java
+++ DateExamples/LongTest.java
@@ -1,12 +1,16 @@
 // package Friday13;   removed to make compile and run 
-import java.util.stream.LongStream;
-import java.text.SimpleDateFormat;
-import java.util.*;
+//import java.util.stream.LongStream;
+//import java.text.SimpleDateFormat;
+//import java.util.*;
 public class LongTest {
 public static void main (String args[]) {
 
 String personalLoan = new String("cheap personal loans");
 String homeLoan     = new String("cheap personal loans");
+  
+Integer testInt1 = new Integer(5);  
+Integer testInt2 = new Integer(5);
+
      
 //since two strings are different object result should be false
 boolean result = personalLoan == homeLoan;
@@ -16,5 +20,7 @@ System.out.println("Comparing two strings with == operator: " + result);
 result = personalLoan.equals(homeLoan);
 System.out.println("Comparing two Strings with same content using equals method: " + result);
 
+result = testInt1.equals(testInt2);
+System.out.println("Comparing two Integers with same content using equals method: " + result);
 }
 }
