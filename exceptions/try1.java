ab8802bf119818d0baa307ddf291767b74488404
Mike on Tecra <matroxmike@icloud.com>
2020-08-16 17:08:47 +1000

    demo working - catch

--- exceptions/try1.java
+++ exceptions/try1.java
@@ -1,19 +1,22 @@
 public class try1{
 	public static void main(String[] args) {
 
-    int pay=8,payda=0;  
-
+    int  pay=8;  // payda=0;  
+	double result = 2.2;
+	for (int payda = 5; payda > -5 ; payda=payda-1) {
     try {
 
-        double result=pay/(double)payda; // if I cast any of the two variables, program does not recognize the catch block, why is it so?
-        System.out.println(result);
+        result = pay/payda; // if I cast any of the two variables, program does not recognize the catch block, why is it so?
+        System.out.println(payda + "  result 1  " + result );
         System.out.println("inside-try");
 
     } catch (Exception e) {
 
-        System.out.println("division by zero exception");
-        System.out.println("inside-catch");
+        System.out.println("division by zero exception " + "inside-catch");
+        //System.out.println("inside-catch");
 
-    }
+		}
+		System.out.println(payda + "  result 2  " + result);
+	}
 }
 }
