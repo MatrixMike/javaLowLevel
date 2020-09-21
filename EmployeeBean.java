c80c5f997f8458d8c16a13aabe9a70dcb7dad4ed
MatroxMike on Optiplex <matroxmike@icloud.com>
2020-09-21 21:37:31 +1000

    added import

---
+++ EmployeeBean.java
@@ -0,0 +1,31 @@
+import java.io.Serializable;
+import java.time.LocalDate;
+
+
+public class EmployeeBean implements Serializable {
+	 
+	    private static final long serialVersionUID = -3760445487636086034L;
+	    private String firstName;
+	    private String lastName;
+	    private LocalDate startDate;
+	 
+	    public EmployeeBean() {
+	    }
+	 
+	    public EmployeeBean(String firstName, String lastName, LocalDate startDate) {
+	        this.firstName = firstName;
+	        this.lastName = lastName;
+	        this.startDate = startDate;
+	    }
+	 
+	    public String getFirstName() {
+	        return firstName;
+	    }
+	 
+	    public void setFirstName(String firstName) {
+	        this.firstName = firstName;
+	    }
+	 
+	    //  additional getters/setters
+	 
+	}
