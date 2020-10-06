5ab371b8caf14792f25f76f02dac5a049fbfe249
mikeh (ASUS) <matroxmike@icloud.com>
2020-10-06 23:56:55 +1100

    List Collections sort

---
+++ listObj/ListTest1.java
@@ -0,0 +1,22 @@
+/*
+ * ListTest1.java
+ * 
+ * Copyright 2020 Michael <mikeh@mikeh-K52N>
+06.10.2020 23:52:04
+ * 
+ * 
+ */
+import java.util.Collections;
+import java.util.ArrayList;
+
+public class ListTest1 {
+	
+	public static void main (String[] args) {
+		final ArrayList<Double> drealv = new ArrayList<>();
+        drealv.add(2.1);
+        drealv.add(-3.4);
+        drealv.add(3.1);
+        Collections.sort(drealv);
+	}
+}
+
