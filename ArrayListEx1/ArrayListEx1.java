a2bdb7d5a23f6d07703279793cb233d877722d5e
MatroxMike on Optiplex <matroxmike@icloud.com>
2020-10-09 19:16:51 +1100

    staged conversion

---
+++ ArrayListEx1/ArrayListEx1.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+public class ArrayListEx1 {
+    public static void main(String args[]){
+		    String[] Dogs = {"Greyhounds","boxers","dobermans","Husky",
+            "Labrador", "Chow Chow", " Beagle ",
+            " Rottweiler ", " Doberman ", "Chihuahua",
+            "bull dog","cavalier King Charles Spaniel",
+            " Pit Bull ", "Pug", "Golden Retriever ",
+            "German Shepherd", "Great Dane", "Boxer"};
+            
+	String num = "22,33,44,55,66,77";
+	String str[] = num.split(",");
+	
+	List<String> al = new ArrayList<String>();
+	al = Arrays.asList(Dogs);
+	for(String s: al){
+	   System.out.println(s);
+	}
+   }
+}
