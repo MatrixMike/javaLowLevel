95c31bbdc2e69b995f84be5b41c250a449746713
Mike Hewitt <matroxmike@icloud.com>
2021-04-08 16:49:44 +1000

    something about formatting numbers
    something about formatting numbers

--- locale/NumberF.java
+++ locale/NumberF.java
@@ -1,14 +1,14 @@
 /*
  * NumberF.java
- * 
+ *
  * Copyright 2016 Michael Hewitt <mikeh@electroteach.com>
  * 17.08.2016
  * curious about :
  * why so many import statements needed,
- * 
- * 
+ *
+ *
  * lesson for me here is: use the https://docs.oracle.com/javase/8/docs/api/
- * 
+ *
  */
 import java.text.Format;
 import java.text.NumberFormat;
@@ -16,30 +16,34 @@ import java.text.DecimalFormat;
 import java.util.Locale;
 
 public class NumberF {
-	
+
 	public static void main (String args[]) {
 // Print out a number using the localized number, integer, currency,
 // and percent format for each locale</strong>
  Locale[] localesN = DecimalFormat.getAvailableLocales(); // appears same as NumberFormat
  double myNumber = -1234.56789;
  NumberFormat form;
- 
- 
+
+
  NumberFormat nf = NumberFormat.getInstance();
  /*for (int i = 0; i < myNumber.length; ++i) {
      output.println(nf.format(myNumber[i]) + "; ");
  }
  */
- 
+
  for (int j = 0; j < 4; ++j) {
+	 System.out.println();
      System.out.println("<<<<< FORMAT >>>"+j+">>");
+     System.out.println();
      for (int i = 0; i < localesN.length; ++i) {
 		 /*
          if (localesN[i].getCountry().length() == 0) {
             continue; // Skip language-only locales
          }
          */
-         System.out.print("<"+i+"> "+ localesN[i].getDisplayName());
+
+         System.out.format("%3d ",i);
+         System.out.print( localesN[i].getDisplayName());    // "<"+i+"> "+
          switch (j) {
          case 0:
              form = NumberFormat.getInstance(localesN[i]); break;
@@ -50,7 +54,7 @@ public class NumberF {
          default:
              form = NumberFormat.getPercentInstance(localesN[i]); break;
          }
-         if (form instanceof DecimalFormat) { 
+         if (form instanceof DecimalFormat) {
 // see http://docs.oracle.com/cd/E26806_01/wlp.1034/e14255/com/bea/p13n/expression/operator/Instanceof.html
              System.out.print(":: " + ((DecimalFormat) form).toPattern());
          }
@@ -64,7 +68,7 @@ public class NumberF {
      }
  }
 	}
-	
+
 
 }
 
